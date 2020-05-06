86f19bc676698324e3ca4b9293abd53cf88cf121
Alexei Spirit <alexeispirit@gmail.com>
2020-05-06 18:43:10 +0500

    19-converting-styling-to-jsx-format

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -4,7 +4,17 @@ import ReactDOM from "react-dom";
 
 // Create a react component
 const App = () => {
-  return <div>Hi there!</div>;
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        Enter name:
+      </label>
+      <input type="text" id="name" />
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        Submit
+      </button>
+    </div>
+  );
 };
 
 // Take the react component and show it on the screen
