88b9a82ccc76d8152017d69c6cdaf1662d693bb8
Alexei Spirit <alexeispirit@gmail.com>
2020-05-07 17:17:13 +0500

    85-handling-form-submit

--- 04-pics/src/components/SearchBar.js
+++ 04-pics/src/components/SearchBar.js
@@ -1,15 +1,27 @@
 import React from "react";
 
 class SearchBar extends React.Component {
-  onInputChange() {}
+  state = { term: "" };
+
+  onInputChange(event) {
+    this.setState({ term: event.target.value });
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+  }
 
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.onSubmit.bind(this)}>
           <div className="field">
             <label htmlFor="">Image Search</label>
-            <input type="text" onChange={this.onInputChange} />
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={this.onInputChange.bind(this)}
+            />
           </div>
         </form>
       </div>
