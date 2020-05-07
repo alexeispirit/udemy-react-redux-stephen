97973252d2122f01f1b084bdf2a872318ee12b96
Alexei Spirit <alexeispirit@gmail.com>
2020-05-07 17:40:44 +0500

    89-invoking-callback-in-children

--- 04-pics/src/components/SearchBar.js
+++ 04-pics/src/components/SearchBar.js
@@ -7,14 +7,15 @@ class SearchBar extends React.Component {
     this.setState({ term: event.target.value });
   };
 
-  onSubmit = (event) => {
+  onFormSubmit = (event) => {
     event.preventDefault();
+    this.props.onSubmit(this.state.term);
   };
 
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form" onSubmit={this.onSubmit}>
+        <form className="ui form" onSubmit={this.onFormSubmit}>
           <div className="field">
             <label htmlFor="">Image Search</label>
             <input
