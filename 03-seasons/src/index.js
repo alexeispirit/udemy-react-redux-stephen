c82b8b58bc4df68e21165e3adc091c5789a696d1
Alexei Spirit <alexeispirit@gmail.com>
2020-05-07 09:21:18 +0500

    62-refactoring-data-loading-to-lifecycle-mehtods

--- 03-seasons/src/index.js
+++ 03-seasons/src/index.js
@@ -5,7 +5,9 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { lat: null, errorMessage: "" };
+  }
 
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       (position) => this.setState({ lat: position.coords.latitude }),
       (err) => this.setState({ errorMessage: err.message })
