47bc51e39f37ee123107d7bd24e0a40602eefb17
Alexei Spirit <alexeispirit@gmail.com>
2020-05-07 18:48:06 +0500

    101-implementing-keys-in-lists

--- 04-pics/src/components/ImageList.js
+++ 04-pics/src/components/ImageList.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 const ImageList = (props) => {
-  const images = props.images.map((image) => {
-    return <img src={image.urls.regular} alt="car" />;
+  const images = props.images.map(({ id, urls, description }) => {
+    return <img src={urls.regular} alt={description} key={id} />;
   });
 
   return <div>{images}</div>;
