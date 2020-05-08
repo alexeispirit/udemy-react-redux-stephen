c2cf8f6b8138de64af208e072b065ace7204e234
Alexei Spirit <alexeispirit@gmail.com>
2020-05-08 19:23:51 +0500

    180-dispatching-correct-values

--- 07-blog/src/actions/index.js
+++ 07-blog/src/actions/index.js
@@ -3,5 +3,5 @@ import jsonplaceholder from "../api/jsonplaceholder";
 export const fetchPosts = () => async (dispatch) => {
   const response = await jsonplaceholder.get("/posts");
 
-  dispatch({ type: "FETCH_POSTS", payload: response });
+  dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
