0c118fe72cf53afbfd40126e70bb20e988ef8c69
Alexei Spirit <alexeispirit@gmail.com>
2020-05-09 10:16:23 +0500

    209-connecting-header

--- 08-streams/client/src/components/App.js
+++ 08-streams/client/src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 
+import Header from "./Header";
 import StreamCreate from "./streams/StreamCreate";
 import StreamDelete from "./streams/StreamDelete";
 import StreamEdit from "./streams/StreamEdit";
@@ -9,16 +10,19 @@ import StreamShow from "./streams/StreamShow";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <div>
-        <Route path="/" exact component={StreamList} />
-        <Route path="/streams/new" exact component={StreamCreate} />
-        <Route path="/streams/edit" exact component={StreamEdit} />
-        <Route path="/streams/delete" exact component={StreamDelete} />
-        <Route path="/streams/show" exact component={StreamShow} />
-      </div>
-      ;
-    </BrowserRouter>
+    <div className="ui container">
+      <Header />
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
+        </div>
+        ;
+      </BrowserRouter>
+    </div>
   );
 };
 
