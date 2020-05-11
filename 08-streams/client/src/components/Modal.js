1e96d1251cf8e2f93d7413f8968d5d94631840fa
Alexei Spirit <alexeispirit@gmail.com>
2020-05-11 10:49:12 +0500

    278-hiding-modal

--- 08-streams/client/src/components/Modal.js
+++ 08-streams/client/src/components/Modal.js
@@ -1,13 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import history from "../history";
 
 const Modal = (props) => {
   return ReactDOM.createPortal(
-    <div className="ui dimmer modals visible active">
-      <div className="ui strandard modal visible active">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia earum
-        perferendis, quam magni numquam veniam minus? Distinctio numquam minima
-        deserunt.
+    <div
+      className="ui dimmer modals visible active"
+      onClick={() => history.push("/")}
+    >
+      <div
+        className="ui strandard modal visible active"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <div className="header">Delete Stream</div>
+        <div className="content">
+          Are your sure you want to delete this stream?
+        </div>
+        <div className="actions">
+          <button className="ui primary button">Delete</button>
+          <button className="ui button">Cancel</button>
+        </div>
       </div>
     </div>,
     document.getElementById("modal")
